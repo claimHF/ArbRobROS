df3af9f4821e8f979b4612d2c61fc710d2e1c0c5
claimhf <h.flothmann@claimspace.de>
2022-02-14 15:30:16 +0100

    add apt-key and updates apt befor install

--- docker/moveit-arbrob/scripts/premake.sh
+++ docker/moveit-arbrob/scripts/premake.sh
@@ -16,6 +16,12 @@ if [ ! -d "$CATKIN_WS/src/urdf_tutorial" ]; then
   git clone https://github.com/ros/urdf_tutorial.git
 fi
 
+# Fix ros key
+# NOTE: Needed because of this bug https://github.com/osrf/docker_images/issues/535
+apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+apt-get update
+
 # Install dependencies
 apt-get install -y ros-${ROS_DISTRO}-joint-state-publisher ros-${ROS_DISTRO}-joint-state-publisher-gui ros-${ROS_DISTRO}-industrial-core ros-${ROS_DISTRO}-robot-state-publisher liburdfdom-tools
 apt-get install -y nano
