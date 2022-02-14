698ea80f1287ce98ba413c3a22b394242e5b7ee3
claimhf <h.flothmann@claimspace.de>
2022-02-14 14:04:13 +0100

    initial robo set up via catkin inside docker

--- docker/moveit-arbrob/scripts/premake.sh
+++ docker/moveit-arbrob/scripts/premake.sh
@@ -6,9 +6,16 @@ cd $CATKIN_WS/src
 if [ ! -d "$CATKIN_WS/src/ros_file_server" ]; then
   git clone https://github.com/gramaziokohler/ros_file_server.git
 fi
+
+# get UR
 if [ ! -d "$CATKIN_WS/src/universal_robot" ]; then
   git clone -b ${ROS_DISTRO}-devel https://github.com/ros-industrial/universal_robot.git
 fi
 
+if [ ! -d "$CATKIN_WS/src/urdf_tutorial" ]; then
+  git clone https://github.com/ros/urdf_tutorial.git
+fi
+
 # Install dependencies
-apt-get install -y ros-${ROS_DISTRO}-joint-state-publisher ros-${ROS_DISTRO}-industrial-core
+apt-get install -y ros-${ROS_DISTRO}-joint-state-publisher ros-${ROS_DISTRO}-joint-state-publisher-gui ros-${ROS_DISTRO}-industrial-core ros-${ROS_DISTRO}-robot-state-publisher liburdfdom-tools
+apt-get install -y nano
